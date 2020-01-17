e2d5ae032694f77e8ff9306b336e32c906e87962
Enrique Díaz <endiba360@gmail.com>
2020-01-16 18:58:39 -0600

    Example of how we can send props

--- 03 Props and More/01 Hello Props/Hello.js
+++ 03 Props and More/01 Hello Props/Hello.js
@@ -1,8 +1,9 @@
 class Hello extends React.Component{
     render(){
+        var exclamation = "!".repeat(this.props.exclamation)
         return (
             <div>
-                <h1>Hello!!</h1>
+                <h1>Hello {this.props.to} from {this.props.from} {exclamation}</h1>
             </div>
         );
     }
